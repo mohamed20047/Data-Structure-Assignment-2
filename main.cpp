0edadd8a90febc77b866809d47318ab0274ac4d5
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2024-05-18 18:53:58 +0300

    Add files via upload
    uplading main

--- main.cpp
+++ main.cpp
@@ -44,11 +44,72 @@ bool compByName(Item i1, Item i2) {
     return i1.get_name() < i2.get_name();
 }
 
-int main() {
-    Item item("ahmed", "sss", 100);
-    ifstream file("../input.txt");
-    AVLTree<Item> *tree = new AVLTree<Item>(item);
-    readTreeItems(file, tree);
-    tree->erase(tree, item);
-    tree->printInOrder(tree);
+void  menu (){
+    cout << "=========================================================================\n";
+    cout << "Enter your name please : ";
+    string name;
+    cin >> name ;
+    cout << "=========================================================================\n\n";
+
+    cout << "Welcom to our application " << name << endl << endl ;
+    cout << "=========================================================================\n\n";
+
+    cout << "We have three types of tree data structures\n\n=========================================================================\n";
+    cout <<"(i) Binary Search Trees (BST)\n(ii) Heaps\n(iii) AVL Trees\nYou can choose any type to performe some operations\n";
+    cout << "You can press ( i , ii , iii)\n";
+    cin >> type;
+    transform(type.begin(), type.end(), type.begin(),:: toupper);
+    cout << "=========================================================================\n";
+    cout << "=========================================================================\n";
+    cout << "This is the operations that applied the tree structures.\n";
+    cout << "=========================================================================\n";
+    
 }
+
+void minimenu(){
+    cout << "I.Add item data.\n";
+    cout << "II. Remove item data.\n";
+    cout << "III. Display the item data normally.\n";
+    cout << "IV. Display all the items sorted by their name ascending.\n";
+    cout << "V. Display all the items sorted by their name descending.\n";
+    cout << "VI. Display all the items sorted by their prices ascending.\n";
+    cout << "VII. Display all the items sorted by their prices descending.\n";
+    cout << "Press Q to exit from the program.\n";
+    cout << "You can press ( I , II , III, IV, V, VI, VII)\n";
+    cin >> operation;
+    transform(operation.begin(), operation.end(), operation.begin(),:: toupper);
+    if ( operation == "I" && type == "i" ){
+    }
+}
+
+string operation = "";
+string type ;
+
+
+int main() {
+    Item item("test1", "sss", 100);
+    ifstream file("../input.txt");
+    // AVLTree<Item> *tree = new AVLTree<Item>(item);
+    // tree->erase(tree, item);
+    // tree->printInOrder(tree);
+    menu();
+    // bst
+    if ( type == "I"){
+        BST_Tree<Item> bst_price = new BST_Tree<Item>(item);
+        BST_Tree<Item> bst_name = new BST_Tree<Item>(item);
+        readTreeItems(file, bst);
+        bst->erase(bst,item);
+    }
+    // heap
+    else if ( type == "II"){
+
+    }
+    // AVL
+    else {
+
+    }
+
+
+    
+}
+
